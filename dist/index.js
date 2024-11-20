e1e4d101685d9ccedc27fc21b15f4de5554fb29d
Nischay Sinha <nischey.me008@gmail.com>
2024-11-20 23:23:17 +0530

    Added on 20 November 2024

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,17 @@
 "use strict";
-// let a:string="23";
-// let check:boolean;
-// let nambo:number=343;
-let surname; //Union Type
-surname = "asfas";
-surname = 123;
-let a;
-const func = (z, x) => {
-    console.log(z, x);
-    return z + x;
+// // let a:string="23";
+// // let check:boolean;
+// // let nambo:number=343;
+// let surname:string| number;  //Union Type
+// surname="asfas";
+// surname=123;
+// // const func=(m:number,n:number)=>{
+// // console.log(m,n);
+// // }
+// type UserName=string| number;
+// type UserName2=(n:string,m:String)=>String;
+const myObj = {
+    name: "Abhi",
+    email: "nischay@gmail.com"
 };
-const Nischay = func("Nischay", "Sinha");
-// console.log(Nischay)
-const arr = [23, 24, 25, 26, 27, 28, 29];
-const arr2 = ["n", "s"];
-const obj = {
-    height: 3434,
-    weight: 34,
-    gender: true
-};
-const obj3 = {
-    height: 3434,
-    weight: 34,
-};
+console.log(myObj.name);
